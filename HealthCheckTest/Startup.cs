9bedea74578e580fb3bcdf3a52c71b88da02763a
IanChen <ian806.chen@gmail.com>
2020-03-04 09:50:12 +0800

    add health checks.

--- HealthCheckTest/Startup.cs
+++ HealthCheckTest/Startup.cs
@@ -26,6 +26,8 @@ namespace HealthCheckTest
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+
+            services.AddHealthChecks();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -45,6 +47,7 @@ namespace HealthCheckTest
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllers();
+                endpoints.MapHealthChecks("/Test");
             });
         }
     }
